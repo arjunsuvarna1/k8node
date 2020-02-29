8dae16a06ed1d7da774ea7f26ba926fcc9aa1ac8
arjunsuvarna1 <4sf15cs020@sahyadri.edu.in>
2020-02-29 22:21:11 +0530

    build test2

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/',(req,res)=>res.send('Hello Kubernetes'))
+app.get('/',(req,res)=>res.send('Hello From Kubernetes Cluster'))
 
 app.listen(port, ()=>console.log(`Example app listening on port ${port}`))
